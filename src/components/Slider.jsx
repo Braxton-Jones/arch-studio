e85ddb09f3bbc25c111564211c25c5c1beacd6f5
Braxton <65457602+braxttsl-dev@users.noreply.github.com>
2023-03-17 19:05:18 -0400

    "IMAGES"

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useImport } from 'use-import';
+import data from '../assets/data.json';
 import '../sass/components/Slider.scss';
 
 function Slider() {
 	const [currentSlide, setCurrentSlide] = useState(0);
-
-	// Dynamically load data file using useImport hook
-	const [slides] = useImport(() =>
-		import('../assets/data.json').then((module) => module.default.home),
-	);
+	const slides = data.home;
 
 	const handleSlideChange = (index) => {
 		setCurrentSlide(index);
@@ -17,37 +13,35 @@ function Slider() {
 
 	return (
 		<div className='img-slider'>
-			{slides &&
-				slides.map((slide, index) => (
+			{slides.map((slide, index) => (
+				<div
+					key={index}
+					className={`slide ${index === currentSlide ? 'active' : ''}`}
+					style={{
+						backgroundImage: `url(${
+							import.meta.env.BASE_URL
+						}assets/home/desktop/${slide.image})`,
+					}}
+				>
+					<div className='info'>
+						<h2>{slide.name}</h2>
+						<p>{slide.desc}</p>
+						<Link to='/portfolio' className='slider-link'>
+							<button className='button slider-btn'>See Our Portfolio</button>
+						</Link>
+					</div>
+				</div>
+			))}
+			<div className='navigation'>
+				{slides.map((slide, index) => (
 					<div
 						key={index}
-						className={`slide ${index === currentSlide ? 'active' : ''}`}
-						style={{
-							backgroundImage: `url(${
-								require(`../assets/home/desktop/${slide.image}`).default
-							})`,
-						}}
+						className={`btn ${index === currentSlide ? 'active' : ''}`}
+						onClick={() => handleSlideChange(index)}
 					>
-						<div className='info'>
-							<h2>{slide.name}</h2>
-							<p>{slide.desc}</p>
-							<Link to='/portfolio' className='slider-link'>
-								<button className='button slider-btn'>See Our Portfolio</button>
-							</Link>
-						</div>
+						0{index + 1}
 					</div>
 				))}
-			<div className='navigation'>
-				{slides &&
-					slides.map((slide, index) => (
-						<div
-							key={index}
-							className={`btn ${index === currentSlide ? 'active' : ''}`}
-							onClick={() => handleSlideChange(index)}
-						>
-							0{index + 1}
-						</div>
-					))}
 			</div>
 		</div>
 	);
