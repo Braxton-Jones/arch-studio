06cebb404e5d83cba4645eb8c9f763622dc61fbd
Braxton <65457602+braxttsl-dev@users.noreply.github.com>
2023-03-17 19:42:22 -0400

    "images"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,9 @@ function Home(props){
 		{
 			id: 1,
 			imageSources: {
-				mobile: require('.././src/assets/portfolio/mobile/image-seraph.jpg'),
-				tablet: require('.././src/assets/portfolio/tablet/image-seraph.jpg'),
-				desktop: require('.././src/assets/portfolio/desktop/image-seraph.jpg'),
+				mobile: import('.././src/assets/portfolio/mobile/image-seraph.jpg'),
+				tablet: import('.././src/assets/portfolio/tablet/image-seraph.jpg'),
+				desktop: import('.././src/assets/portfolio/desktop/image-seraph.jpg'),
 			},
 			title: 'Seraph Station',
 			date: 'September 2019',
@@ -17,7 +17,7 @@ function Home(props){
 		{
 			id: 2,
 			imageSources: {
-				mobile: require('./src/assets/portfolio/mobile/image-eebox.jpg'),
+				mobile: import('./src/assets/portfolio/mobile/image-eebox.jpg'),
 				tablet: require('./src/assets/portfolio/tablet/image-eebox.jpg'),
 				desktop: require('./src/assets/portfolio/desktop/image-eebox.jpg'),
 			},
