bba8345aac15f69f16a3a74e09aa687f937ff455
Braxton <65457602+braxttsl-dev@users.noreply.github.com>
2023-03-17 18:38:41 -0400

    "images"

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,58 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from "../assets/data.json"
-import "../sass/components/Slider.scss"
+import data from '../data.json';
+import '../sass/components/Slider.scss';
 
 function Slider() {
-  const [currentSlide, setCurrentSlide] = useState(0);
+	const [currentSlide, setCurrentSlide] = useState(0);
+	const slides = data.home;
 
-  const handleSlideChange = (index) => {
-    setCurrentSlide(index);
-  };
+	const handleSlideChange = (index) => {
+		setCurrentSlide(index);
+	};
 
-  const renderSlide = async (slide) => {
-    const { image, name, desc } = slide;
-    const { default: backgroundImage } = await import(`../assets/home/desktop/${image}`);
-
-    return (
-      <div
-        key={image}
-        className={`slide ${slides.indexOf(slide) === currentSlide ? 'active' : ''}`}
-        style={{ backgroundImage: `url(${backgroundImage})` }}
-      >
-        <div className='info'>
-          <h2>{name}</h2>
-          <p>{desc}</p>
-          <Link to='/portfolio' className='slider-link'>
-            <button className='button slider-btn'>See Our Portfolio</button>
-          </Link>
-        </div>
-      </div>
-    );
-  };
-
-  const slides = [
-    { image: 'image1.jpg', name: 'Slide 1', desc: 'Description 1' },
-    { image: 'image2.jpg', name: 'Slide 2', desc: 'Description 2' },
-    { image: 'image3.jpg', name: 'Slide 3', desc: 'Description 3' },
-  ];
-
-  return (
-    <div className='img-slider'>
-      {slides.map((slide) => renderSlide(slide))}
-      <div className='navigation'>
-        {slides.map((slide, index) => (
-          <div
-            key={slide.image}
-            className={`btn ${index === currentSlide ? 'active' : ''}`}
-            onClick={() => handleSlideChange(index)}
-          >
-            0{index + 1}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className='img-slider'>
+			{slides.map((slide, index) => (
+				<div
+					key={index}
+					className={`slide ${index === currentSlide ? 'active' : ''}`}
+					style={{
+						backgroundImage: `url(/assets/home/desktop/${slide.image})`,
+					}}
+				>
+					<div className='info'>
+						<h2>{slide.name}</h2>
+						<p>{slide.desc}</p>
+						<Link to='/portfolio' className='slider-link'>
+							<button className='button slider-btn'>See Our Portfolio</button>
+						</Link>
+					</div>
+				</div>
+			))}
+			<div className='navigation'>
+				{slides.map((slide, index) => (
+					<div
+						key={index}
+						className={`btn ${index === currentSlide ? 'active' : ''}`}
+						onClick={() => handleSlideChange(index)}
+					>
+						0{index + 1}
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Slider;
+
+
--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	plugins: [react()],
 	base: '/arch-studio/',
-	build: {
-		rollupOptions: {
-			output: {
-				assetFileNames: 'assets/[name].[hash][extname]',
-			},
-		},
-	},
 });
