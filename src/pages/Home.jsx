f02286325f0709842774772e646a136e35db73e2
Braxton <65457602+braxttsl-dev@users.noreply.github.com>
2023-03-17 20:36:47 -0400

    images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,54 +1,34 @@
 import "../sass/pages/_Home.scss"
 import {Link} from 'react-router-dom';
 import Slider from "../components/Slider";
+import delSolMobile from '../assets/portfolio/mobile/image-del-sol.jpg';
+import delSolTablet from '../assets/portfolio/tablet/image-del-sol.jpg';
+import delSolDesktop from '../assets/portfolio/desktop/image-del-sol.jpg';
+
+import prototypeMobile from '../assets/portfolio/mobile/image-prototype.jpg';
+import prototypeTablet from '../assets/portfolio/tablet/image-prototype.jpg';
+import prototypeDesktop from '../assets/portfolio/desktop/image-prototype.jpg';
+
+import tower228bMobile from '../assets/portfolio/mobile/image-228b.jpg';
+import tower228bTablet from '../assets/portfolio/tablet/image-228b.jpg';
+import tower228bDesktop from '../assets/portfolio/desktop/image-228b.jpg';
+
+
 function Home(props){
 	const image = [
 		{
 			id: 1,
 			imageSources: {
-				mobile: import('../assets/portfolio/mobile/image-seraph.jpg'),
-				tablet: import('../assets/portfolio/tablet/image-seraph.jpg'),
-				desktop: import('../assets/portfolio/desktop/image-seraph.jpg'),
-			},
-			title: 'Seraph Station',
-			date: 'September 2019',
-		},
-
-		{
-			id: 2,
-			imageSources: {
-				mobile: import('../assets/portfolio/mobile/image-eebox.jpg'),
-				tablet: '../src/assets/portfolio/tablet/image-eebox.jpg',
-				desktop: '../src/assets/portfolio/desktop/image-eebox.jpg',
-			},
-			title: 'Eebox building',
-			date: 'August 2017',
-		},
-
-		{
-			id: 3,
-			imageSources: {
-				mobile: import('../assets/portfolio/mobile/image-federal.jpg'),
-				tablet: import('../assets/portfolio/tablet/image-federal.jpg'),
-				desktop: import('../assets/portfolio/desktop/image-federal.jpg'),
-			},
-			title: 'Federal II Tower',
-			date: 'March 2017',
-		},
-
-		{
-			id: 4,
-			imageSources: {
-				mobile: import('../assets/portfolio/mobile/image-del-sol.jpg'),
-				tablet: import('../assets/portfolio/tablet/image-del-sol.jpg'),
-				desktop: import('../assets/portfolio/desktop/image-del-sol.jpg'),
+				mobile: delSolMobile,
+				tablet: delSolTablet,
+				desktop: delSolDesktop,
 			},
 			title: 'Project Del Sol',
 			date: 'January 2016',
 		},
 
 		{
-			id: 5,
+			id: 2,
 			imageSources: {
 				mobile: 'src/assets/portfolio/mobile/image-prototype.jpg',
 				tablet: 'src/assets/portfolio/tablet/image-prototype.jpg',
@@ -59,7 +39,7 @@ function Home(props){
 		},
 
 		{
-			id: 6,
+			id: 3,
 			imageSources: {
 				mobile: 'src/assets/portfolio/mobile/image-228b.jpg',
 				tablet: 'src/assets/portfolio/tablet/image-228b.jpg',
@@ -67,75 +47,9 @@ function Home(props){
 			},
 			title: '228B Tower',
 			date: 'April 2015',
-		},
-
-		{
-			id: 7,
-			imageSources: {
-				mobile: 'src/assets/portfolio/mobile/image-edelweiss.jpg',
-				tablet: 'src/assets/portfolio/tablet/image-edelweiss.jpg',
-				desktop: 'src/assets/portfolio/desktop/image-edelweiss.jpg',
-			},
-			title: 'Grand Edelweiss',
-			date: 'December 2013',
-		},
-
-		{
-			id: 8,
-			imageSources: {
-				mobile: './src/assets/portfolio/mobile/image-netcry.jpg',
-				tablet: './src/assets/portfolio/tablet/image-netcry.jpg',
-				desktop: './src/assets/portfolio/desktop/image-netcry.jpg',
-			},
-			title: 'Netcry Tower',
-			date: 'August 2012',
-		},
-
-		{
-			id: 9,
-			imageSources: {
-				mobile: 'src/assets/portfolio/mobile/image-hypers.jpg',
-				tablet: 'src/assets/portfolio/tablet/image-hypers.jpg',
-				desktop: 'src/assets/portfolio/desktop/image-hypers.jpg',
-			},
-			title: 'Hypers',
-			date: 'January 2012',
-		},
-
-		{
-			id: 10,
-			imageSources: {
-				mobile: 'src/assets/portfolio/mobile/image-sxiv.jpg',
-				tablet: 'src/assets/portfolio/tablet/image-sxiv.jpg',
-				desktop: 'src/assets/portfolio/desktop/image-sxiv.jpg',
-			},
-			title: 'SXIV Tower',
-			date: 'March 2011',
-		},
-
-		{
-			id: 11,
-			imageSources: {
-				mobile: 'src/assets/portfolio/mobile/image-trinity.jpg',
-				tablet: 'src/assets/portfolio/tablet/image-trinity.jpg',
-				desktop: 'src/assets/portfolio/desktop/image-trinity.jpg',
-			},
-			title: 'Trinty Bank Tower',
-			date: 'September 2010',
-		},
-
-		{
-			id: 12,
-			imageSources: {
-				mobile: './src/assets/portfolio/mobile/image-paramour.jpg',
-				tablet: './src/assets/portfolio/tablet/image-paramour.jpg',
-				desktop: './src/assets/portfolio/desktop/image-paramour.jpg',
-			},
-			title: 'Project Paramour',
-			date: 'February 2008',
-		},
+		}
 	];
- const imageshort = image.slice(3, 6)
+ const imageshort = image
  console.log(imageshort[1].imageSources[props.deviceBreakpoint]);
     return (
 			<section className='home'>
